a67e31f1a70c746fa0c7c78fb2b0833478c5c899
aidan-brown <abrownie99600@gmail.com>
2019-11-19 14:08:52 -0500

    Added a pokemon list

--- js/pokeApi.js
+++ js/pokeApi.js
@@ -1,9 +1,10 @@
-let limit = 25;
+let pokeElements = new Array();
+let limit = 807;
 
-function getPokemon(page){
+function getPokemon(){
     let content = document.querySelector('#content');
 
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${page * limit}`;
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}`;
 
     let req = new XMLHttpRequest();
 
@@ -14,88 +15,129 @@ function getPokemon(page){
 
     let data = JSON.parse(req.responseText).results;
 
-    for(let pokemon of data){
-        let pokeReq = new XMLHttpRequest();
+    updateLinks(data);
 
-        pokeReq.onload = createPokeElement;
-        pokeReq.open('get', pokemon.url, false);
-        pokeReq.send();
+    for(let i = 0; i < data.length; i++){
+        createPokeElement(data[i], i + 1);
+        if(i == 1){
+            let dataReq = new XMLHttpRequest();
+
+            req.onload = () => console.log(dataReq.responseText);
+            req.open('get', pokeElements[i].url, true);
+            req.send();
+        }
+    }
+
+    return new Promise(resolve => {
+        resolve("p");
+        console.log('resolved p');
+    });
+}
+
+function createPokeElement(pokeData, id){
+    let pokeElement = {name: pokeData.name[0].toUpperCase() + pokeData.name.slice(1), url: pokeData.url, data: null};
+    if(pokeElement.name[pokeElement.name.length - 2] == '-'){
+        pokeElement.name = pokeElement.name.slice(0, pokeElement.name.length - 1) + pokeElement.name[pokeElement.name.length - 1].toUpperCase();
+    }
+    pokeElements[id] = pokeElement;
+}
+
+async function getPokeData(){
+    const p = getPokemon();
+
+    await p;
+
+    for(let i = 1; i < pokeElements.length; i++){
+        let req = new XMLHttpRequest();
+
+        req.onload = () => setData(i, JSON.parse(req.responseText))
+        req.open('get', pokeElements[i].url, true);
+        req.send();
     }
 }
 
-function createPokeElement(e){
-    let pokeData = JSON.parse(e.target.responseText);
+function setData(index, data){
+    pokeElements[index].data = data;
+}
 
-    console.log(pokeData);
+function updateDisplay(index){
+    let currentPokemon = pokeElements[index];
 
-    let pokeInfo = document.createElement('section');
-    pokeInfo.className = 'poke-info';
-    pokeInfo.id = pokeData.id;
+    let pokeDisplay = document.querySelector('.poke-display');
+    pokeDisplay.innerHTML = '';
 
-        let pokeName = document.createElement('h2');
-        pokeName.classList = 'poke-name';
-        pokeName.innerHTML = `#${pokeData.id} ` + pokeData.name[0].toUpperCase() + pokeData.name.slice(1);
-        pokeInfo.append(pokeName);
+    let pokeName = document.createElement('h2');
+    pokeName.className = 'poke-name';
+    pokeName.innerHTML = currentPokemon.name;
+    pokeDisplay.append(pokeName);
 
+    if(currentPokemon.data){
         let pokeSprite = document.createElement('img');
-        pokeSprite.classList = 'poke-sprite';
-        pokeSprite.src = pokeData.sprites["front_default"];
-        pokeInfo.append(pokeSprite);
-
-        let pokeStats = document.createElement('div');
-        pokeStats.classList = 'poke-stats';
-        for(let i = 0; i < pokeData.stats.length; i++){
-            let statElement = document.createElement('p');
-            statElement.className = pokeData.stats[i].stat.name;
-            statElement.innerHTML = pokeData.stats[i].stat.name[0].toUpperCase() + pokeData.stats[i].stat.name.slice(1) + ': ' + pokeData.stats[i].base_stat;
-            pokeStats.append(statElement);
-        }
-        pokeInfo.append(pokeStats);
+        pokeSprite.className = 'poke-sprite'
+        pokeSprite.src = currentPokemon.data.sprites['front_default'];
+        pokeDisplay.append(pokeSprite);
 
         let pokeTypes = document.createElement('div');
         pokeTypes.className = 'poke-types';
-        for(let i = pokeData.types.length - 1; i >= 0; i--){
-            let typeElement = document.createElement('p');
-            typeElement.className = pokeData.types[i].type.name;
-            typeElement.innerHTML = pokeData.types[i].type.name[0].toUpperCase() + pokeData.types[i].type.name.slice(1);
-            pokeTypes.append(typeElement);
+        for(let i = currentPokemon.data.types.length - 1; i >= 0; i--){
+            let pokeType = document.createElement('p');
+            pokeType.className = currentPokemon.data.types[i].type.name;
+            pokeType.innerHTML = currentPokemon.data.types[i].type.name[0].toUpperCase() + currentPokemon.data.types[i].type.name.slice(1);
+            pokeTypes.append(pokeType);
         }
-        pokeInfo.append(pokeTypes);
+        pokeDisplay.append(pokeTypes);
 
-    content.append(pokeInfo);
+        let pokeStats = document.createElement('p');
+        pokeStats.className = 'poke-stats';
+        for(let i = 0; i < currentPokemon.data.stats.length; i++){
+            pokeStats.innerHTML += `${currentPokemon.data.stats[i].stat.name[0].toUpperCase() + currentPokemon.data.stats[i].stat.name.slice(1)}: ${currentPokemon.data.stats[i].base_stat}<br/>`;
+        }
+        pokeDisplay.append(pokeStats);
+    }
+
+    content.append(pokeDisplay);
 }
 
-let numberOfPokemon;
-let currentPage = 0;
+function updateLinks(data){
+    let pokeLinks = document.querySelector('.poke-links');
+    pokeLinks.innerHTML = '';
 
-let req = new XMLHttpRequest();
-req.open('get', 'https://pokeapi.co/api/v2/pokemon', false);
-req.send();
-numberOfPokemon = JSON.parse(req.responseText).count;
+    for(let i = 0; i < data.length; i++){
+        let pokeLink = document.createElement('p');
+        pokeLink.innerHTML = data[i].name[0].toUpperCase() + data[i].name.slice(1);
 
-let forwardArrows = document.querySelectorAll('.page-forward');
-let backArrows = document.querySelectorAll('.page-back');
-
-for(let button of forwardArrows){
-    button.onclick = () => {
-        if(Math.floor(numberOfPokemon / limit) > currentPage){
-            currentPage++;
-            document.querySelector('#content').innerHTML = '';
-            getPokemon(currentPage);
-            
+        pokeLink.onclick = () => {
+            currentId = i + 1;
         }
-        console.log(numberOfPokemon);
+
+        pokeLinks.append(pokeLink);
     }
 }
 
-for(let button of backArrows){
-    button.onclick = () => {
-        if(0 < currentPage){
-            currentPage--;
-            document.querySelector('#content').innerHTML = '';
-            getPokemon(currentPage);
-        }
-    }
-}
+let currentId = 1;
 
-getPokemon(currentPage);
+let forwardArrow = document.querySelector('.page-forward');
+let backArrow = document.querySelector('.page-back');
+
+forwardArrow.onclick = () => {
+    if(limit > currentId){
+        currentId++;
+
+        updateDisplay(currentId);
+    }
+};
+
+backArrow.onclick = () => {
+    if(1 < currentId){
+        currentId--;
+
+        updateDisplay(currentId);
+    }
+};
+
+
+getPokeData();
+
+setTimeout(() => {
+    updateDisplay(currentId);
+}, 500)
